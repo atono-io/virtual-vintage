43d914f50c93d6abf5d86a90999431019ad1be52
bt-atono <brianna.thomas@atono.io>
2024-06-14 16:39:25 -0700

    add pug & tailwind & template

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ $ DEBUG=myapp:* npm start
 ```
 
 Then, load http://localhost:3000/ in your browser to access the app.
+
+## CSS
+If you add CSS to your application, make sure to run `npm run tailwind:css` each time, so the changes apply. After doing so, start your application, and you should see the changes.
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.pug"],
+  theme: {
+    theme: {
+      colors: {
+        'teal-100': '#91E3ED',
+        'teal-900': '#2D5265',
+        'red-100': '#FFEFEB',
+        // 'gray-100': '#F5F5F5',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
